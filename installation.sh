5e8829e1073aca03ba2bf441797634dc7683d923
senchi <senchpimy@gmail.com>
2022-07-16 20:32:56 -0500

    fixed after deletion

---
+++ installation.sh
@@ -0,0 +1,9 @@
+!/bin/bash
+
+pacman -S awesome curl git wget xorg-server xorg-xinit neovim rofi picom ranger zsh zathura xclip rxvt-unicode python python mpd mpv flameshot noto-fonts-emoji
+git clone https://aur.archlinux.org/yay-git.git && cd yay && makepkg -si
+yay -S pywal-16-colors python-pynvim
+
+
+git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
+echo "default_linemode devicons" >> $HOME/.config/ranger/rc.conf
