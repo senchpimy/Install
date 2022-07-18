28d6b6c333a89e840064defa1da50d6ebee8afe1
senchi <senchpimy@gmail.com>
2022-07-18 14:54:44 -0500

    inconsolata

--- installation.sh
+++ installation.sh
@@ -2,7 +2,7 @@
 
 pacman -S awesome curl git wget xorg-server xorg-xinit neovim rofi picom ranger zsh zathura xclip rxvt-unicode python python mpd mpv flameshot noto-fonts-emoji chafa
 git clone https://aur.archlinux.org/yay-git.git && cd yay && makepkg -si
-yay -S pywal-16-colors python-pynvim ttf-icomoon-feather noto-fonts-emoji ttf-nerd-fonts-symbols
+yay -S pywal-16-colors python-pynvim ttf-icomoon-feather noto-fonts-emoji ttf-nerd-fonts-symbols nerd-fonts-incosolata
 
 
 git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
