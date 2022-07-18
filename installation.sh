f799c1e16acec7115001cde4eace9a795f161480
senchi <senchpimy@gmail.com>
2022-07-17 19:51:03 -0500

    some fonts

--- installation.sh
+++ installation.sh
@@ -1,8 +1,8 @@
 !/bin/bash
 
-pacman -S awesome curl git wget xorg-server xorg-xinit neovim rofi picom ranger zsh zathura xclip rxvt-unicode python python mpd mpv flameshot noto-fonts-emoji
+pacman -S awesome curl git wget xorg-server xorg-xinit neovim rofi picom ranger zsh zathura xclip rxvt-unicode python python mpd mpv flameshot noto-fonts-emoji chafa
 git clone https://aur.archlinux.org/yay-git.git && cd yay && makepkg -si
-yay -S pywal-16-colors python-pynvim
+yay -S pywal-16-colors python-pynvim ttf-icomoon-feather noto-fonts-emoji ttf-nerd-fonts-symbols
 
 
 git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
