4a5a4fa0bf2b1c53c90dfdb1860119fa0a35abe6
senchi <senchpimy@gmail.com>
2022-07-19 12:33:52 -0500

    some changes

--- installation.sh
+++ installation.sh
@@ -1,15 +1,55 @@
-!/bin/bash
+#!/bin/bash
+#slock
+#pip colorz haishoku colorthief fast-colorthief qutebrowser colorama Pillow
+echo "keymap="la-latin1"" | sudo tee -a /etc/conf.d/keymaps
+sudo locale-gen
+pacman -Syu artix-arch-linux-support 
 
-pacman -S awesome curl git wget xorg-server xorg-xinit neovim rofi picom ranger zsh zathura xclip rxvt-unicode python python mpd mpv flameshot noto-fonts-emoji chafa scim
-git clone https://aur.archlinux.org/yay-git.git && cd yay && makepkg -si
-yay -S pywal-16-colors python-pynvim ttf-icomoon-feather noto-fonts-emoji ttf-nerd-fonts-symbols nerd-fonts-incosolata
+echo '
+#[testing]
+#Include = /etc/pacman.d/mirrorlist-arch
+ 
+ 
+[extra]
+Include = /etc/pacman.d/mirrorlist-arch
+ 
+ 
+#[community-testing]
+#Include = /etc/pacman.d/mirrorlist-arch
+ 
+ 
+[community]
+Include = /etc/pacman.d/mirrorlist-arch
+
+
+#[multilib-testing]
+#Include = /etc/pacman.d/mirrorlist-arch
+
+[universe]
+  Server = https://universe.artixlinux.org/$arch
+  Server = https://mirror1.artixlinux.org/universe/$arch
+  Server = https://mirror.pascalpuffke.de/artix-universe/$arch
+  Server = https://artixlinux.qontinuum.space/artixlinux/universe/os/$arch
+  Server = https://mirror1.cl.netactuate.com/artix/universe/$arch
+  Server = https://ftp.crifo.org/artix-universe/
+
+ 
+[multilib]
+Include = /etc/pacman.d/mirrorlist-arch
+ ' |sudo  tee -a /etc/pacman.conf > /dev/null
+
+
+pacman-key --populate archlinux
+pacman -S awesome curl git wget xorg-server xorg-xinit neovim rofi picom ranger zsh zathura xclip rxvt-unicode python python mpd mpv flameshot noto-fonts-emoji chafa scim bluez emacs gimp xcompmgr python-pip ttf-hack fzf fontconfig scim htop youtube-dl
+git clone https://aur.archlinux.org/yay-git.git ~/apps && cd yay && makepkg -si
+yay -S pywal-16-colors python-pynvim ttf-icomoon-feather noto-fonts-emoji ttf-nerd-fonts-symbols nerd-fonts-incosolata tetris-terminal-git tty-clock peerflix czkawka 
 
 git clone https://github.com/senchpimy/awesomewmconfig ~/.config/awesome
 git clone https://github.com/senchpimy/pywal_templates.git ~/.config/wal/
 git clone https://github.com/senchpimy/ranger.git ~/.config/ranger
 git clone https://github.com/senchpimy/nvim.git ~/.config/nvim
 git clone https://github.com/senchpimy/rofi.git ~/.config/rofi
-git clone https://github.com/senchpimy/st.git ~/apps
+git clone https://github.com/senchpimy/st.git ~/apps && cd ~/apps/st && sudo make clean install
 git clone https://github.com/senchpimy/dotfiles.git ~/.config/dotfiles
 mv ~/.config/dotfiles/.zshrc ~/.zshrc
 
